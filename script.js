640f2c02f6724b6e11d4f5ae1f036b4d6abc7d68
AnasFuad <77856292+AnasFuad@users.noreply.github.com>
2022-08-05 15:30:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var menu_id_va = document.getElementById("menu_id")
+let nav_va_2 = document.getElementById("nav_id_2");
+let landing_va = document.getElementById("landing_id");
+let te_ph_id_va = document.getElementById("te-ph_id");
+let location_id_va = document.getElementById("location_id");
+let baking_id_va = document.getElementById("baking_id");
+let contact_id_va = document.getElementById("contact_id");
+let footer_id_va = document.getElementById("footer_id");
+let social_id_va = document.getElementById("social_id");
+
+
+
+
+document.getElementById("menu_id").addEventListener("click",()=>{
+    let nav_va_2 = document.getElementById("nav_id_2");
+
+    nav_va_2.style.top = "115px";
+    nav_va_2.style.display = "block";
+    nav_va_2.style.transition = "0.3s";
+    landing_va.style.position = "realtive";
+    landing_va.style.top = "115px";
+    te_ph_id_va.style.position = "relative";
+    te_ph_id_va.style.top = "128px";
+    location_id_va.style.position = "relative";
+    location_id_va.style.top = "94px"
+
+    baking_id_va.style.position = "relative";
+    baking_id_va.style.top = "94px"
+    contact_id_va.style.position = "relative";
+    contact_id_va.style.top = "94px"
+    footer_id_va.style.position = "relative";
+    footer_id_va.style.top = "94px"
+    social_id_va.style.position = "relative";
+    social_id_va.style.top = "94px"
+
+});
+/*
+document.getElementById("menu_id").addEventListener("click",()=>{
+    if (nav_va_2.sty)
+});*/
+
+/*
+function second() {
+    nav_va_2.style.top = "-500px";
+    nav_va_2.style.display = "none";
+}*/
+
+/*
+if (menu_id_va.onclick = true) {
+    document.getElementById("menu_id").addEventListener("click",()=>{
+        let nav_va_2 = document.getElementById("nav_id_2");
+        nav_va_2.style.background = "red"
+        
+    });
+}else {
+    let nav_va_2 = document.getElementById("nav_id_2");
+    nav_va_2.style.background = "black"
+}*/
+ 
+
